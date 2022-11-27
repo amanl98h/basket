fc84f3995ea554d84595b12cd50159059cd8e896
Aman <amannasyryndekov6@gmail.com>
2022-11-27 14:24:57 +0600

    basket

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { ShoppingCard } from "./components/ShoppingCard";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ShoppingCard />
+    </div>
+  );
+}
